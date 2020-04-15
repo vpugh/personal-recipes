ae945edd9f538506ca08b1ce55e933683b081404
vpugh <hello@toripugh.com>
2020-04-14 23:21:17 -0400

    Add loggedOut homepage content

--- src/components/homepage/logged-out-landing.jsx
+++ src/components/homepage/logged-out-landing.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import CardContainer from '../shared/card-container';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  impactSentence: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: '0 40px',
+  },
+  paraHolder: {
+    maxWidth: '70%',
+    margin: '40px auto',
+    textAlign: 'center',
+    '& > p': {
+      lineHeight: 1.4,
+    },
+  },
+}));
 
 const LoggedOutLanding = () => {
+  const classes = useStyles();
   return (
     <CardContainer>
-      <p>Sign in</p>
+      <div className={`logo serif ${classes.impactSentence}`}>
+        A great place to store readable versions of recipes.
+      </div>
+      <div className={classes.paraHolder}>
+        <p>
+          Honestly, a system I'm making for my mom to save her recipes. If it
+          becomes something cool for others to use, then that's great!
+        </p>
+        <p>
+          Kind of a test if I could build something like this, blame the
+          quarantine.
+        </p>
+        <p>
+          If I get this to work, maybe I can work on porting it to an app, using
+          React Native. Baby steps though, get this working first.
+        </p>
+      </div>
     </CardContainer>
   );
 };
