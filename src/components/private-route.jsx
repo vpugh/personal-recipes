11d9f4001b06ed7ddb278df6ae687fcf0c4756cc
vpugh <hello@toripugh.com>
2020-07-07 20:50:46 -0400

    Update private route

--- src/components/private-route.jsx
+++ src/components/private-route.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useAuth } from '../context/auth-context';
+// import { useAuth } from '../context/auth-context';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Redirect } from 'react-router-dom';
 import PageContainer from './page-container';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { loading, isAuthenticated } = useAuth();
+  const { isLoading, isAuthenticated } = useAuth0();
 
-  if (loading) {
+  if (isLoading) {
     return (
       <Route
         {...rest}
