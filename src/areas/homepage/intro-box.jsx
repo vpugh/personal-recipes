be63c071a452aaf0e30546c65be1af5dc5e8d408
vpugh <hello@toripugh.com>
2020-08-02 13:18:07 -0400

    IntroBox selection can go to recipe

--- src/areas/homepage/intro-box.jsx
+++ src/areas/homepage/intro-box.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import useStyles from '../../styles/landing-styles';
 import { useAuth } from '../../context/auth-context';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const autocompleteStyle = makeStyles((theme) => ({
   root: {
@@ -21,6 +21,8 @@ const autocompleteStyle = makeStyles((theme) => ({
 const IntroBox = () => {
   const classes = useStyles();
   const autoClass = autocompleteStyle();
+  const [value, setValue] = useState();
+  const theme = useTheme();
   const {
     user: { recipes },
   } = useAuth();
@@ -33,6 +35,7 @@ const IntroBox = () => {
           options={recipes || []}
           getOptionLabel={(option) => option.title}
           classes={{ root: autoClass.root }}
+          onChange={(event, newValue) => setValue(newValue)}
           renderInput={(params) => (
             <TextField
               {...params}
@@ -42,6 +45,25 @@ const IntroBox = () => {
             />
           )}
         />
+        {value && (
+          <div style={{ textAlign: 'center' }}>
+            <Link
+              to={`/recipe/${value.id}`}
+              style={{
+                background: theme.palette.primary.main,
+                padding: '10px 30px',
+                textDecoration: 'none',
+                color: 'inherit',
+                marginBottom: 14,
+                display: 'inline-block',
+                fontWeight: 'bold',
+              }}
+              color='primary'
+            >
+              Go To Recipe
+            </Link>
+          </div>
+        )}
       </div>
       <Link to='/add-recipe' className={classes.addNewButton}>
         Add New Recipe
